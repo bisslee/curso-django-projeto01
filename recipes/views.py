cec32e1dc902268a32631498eaed59d644f751fb
Ivana Batista dos Santos <bisslee@gmail.com>
2023-04-19 16:28:29 -0300

    passando valores para o conteudo

--- recipes/views.py
+++ recipes/views.py
@@ -1,18 +1,22 @@
 
 from django.shortcuts import render
 
+from utils.factory import make_recipe
+
 
 # Create your views here.
 def root(request):
     return render(request, 'recipes/pages/home.html', context={
         'name': 'Biss Lee',
-        'greetins': 'Good night'
+        'greetins': 'Good night',
+        'recipes': [make_recipe() for _ in range(11)]
     })
 
 
 def recipe(request, id):
-    return render(request, 'recipes/pages/home.html', context={
+    return render(request, 'recipes/pages/recipe.html', context={
         'name': 'Biss Lee',
         'greetins': 'Good night',
+        'recipe': make_recipe(),
         'id': id
     })
