f89131478ce5e2419ee8520b3c79f2d9951670b0
Ivana Batista dos Santos <bisslee@gmail.com>
2023-04-22 01:35:14 -0300

    Criando model, migrations e alterando paginas

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,7 +2,11 @@ from django.urls import path
 
 from . import views
 
+app_name = 'recipes'
 urlpatterns = [
-    path('', views.root),
-    path('recipes/<int:id>/', views.recipe)
+    path('', views.root, name='home'),
+    path('recipes/category/<int:category_id>/',
+         views.categories,  name='category'),
+    path('recipes/<int:id>/',
+         views.recipe,  name='recipe'),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,22 +1,41 @@
 
-from django.shortcuts import render
+from django.shortcuts import get_list_or_404, get_object_or_404, render
 
 from utils.factory import make_recipe
 
+from .models import Recipe
 
-# Create your views here.
+
 def root(request):
+    recipes = Recipe.objects.filter(is_publish=True).order_by('title')
     return render(request, 'recipes/pages/home.html', context={
         'name': 'Biss Lee',
         'greetins': 'Good night',
-        'recipes': [make_recipe() for _ in range(11)]
+        'recipes': recipes,
+        'is_detail_page': False
+    })
+
+
+def categories(request, category_id):
+    recipes = get_list_or_404(Recipe.objects.filter(
+        category__id=category_id, is_publish=True).order_by('title'))
+
+    category_name = recipes[0].category.name
+    return render(request, 'recipes/pages/category.html', context={
+        'name': 'Biss Lee',
+        'greetins': 'Good night',
+        'recipes': recipes,
+        'is_detail_page': False,
+        'category_name': category_name
     })
 
 
 def recipe(request, id):
+    recipe = get_object_or_404(Recipe, pk=id, is_publish=True)
     return render(request, 'recipes/pages/recipe.html', context={
         'name': 'Biss Lee',
         'greetins': 'Good night',
-        'recipe': make_recipe(),
-        'id': id
+        'recipe': recipe,
+        'id': id,
+        'is_detail_page': True
     })
